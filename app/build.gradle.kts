c6133ebd2b46c807c77cdedab21fb125a2d8aac2
wbham <wbham97@naver.com>
2024-02-13 04:43:23 +0900

    [Feat] - 영상화면 UI. ExoPlayer impl
    - activity_main 에 constraintLayout 및 RecyclerView, BNV 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,9 @@ dependencies {
     // Glide
     implementation("com.github.bumptech.glide:glide:4.15.0")
 
+    // ExoPlayer
+    implementation("com.google.android.exoplayer:exoplayer:2.19.1")
+
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
